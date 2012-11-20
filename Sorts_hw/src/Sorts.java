b17dff422e8d3a667233fff098fbfc20a9b7ff02
alove <alexjlove@earthlink.net>
2012-11-20 09:43:02 -0800

    changed things, added a main description
    Still need to make additional comments for each method.

--- Sorts_hw/src/Sorts.java
+++ Sorts_hw/src/Sorts.java
@@ -1,61 +1,98 @@
 import java.util.Random;
 
-
+/**
+ * 
+ * @author Alex Love
+ * 	This class contains three different sort methods. Bubble sort, selection sort, and insertion sort. 
+ *  Each of these methods takes in an array of integers and sorts them from lowest to highest.
+ *  In bubble sort if an element is greater than the next element, they are swapped.
+ *  In selection sort, the smallest element is found and then placed first. This repeats through all the elements until it is sorted.
+ *  In insertion sort, an element is compared to other elements until all previous elements are less than the current element.
+ *
+ */
 public class Sorts {
 	public static void main(String[] args)
 	{
 		Random rand = new Random();
-		int arrayLength = 15;
-		int[] intArray = new int[arrayLength];
+		int arrayLength = 5;
+		int[] intArrayBubble = new int[arrayLength];
+		int[] intArraySelection = new int[arrayLength];
+		int[] intArrayInsertion = new int[arrayLength];
 		for ( int i = 0; i < arrayLength; i++)
 		{
-			intArray[i] = rand.nextInt(100);
+			intArrayBubble[i] = rand.nextInt(100);
+			intArraySelection[i] = rand.nextInt(100);
+			intArrayInsertion[i] = rand.nextInt(100);
 		}
-		System.out.println("Prior to sort:");
-		for (int i = 0; i < intArray.length ; i++)
+		System.out.println("Before bubble sort:");
+		for (int i = 0; i < arrayLength ; i++)
 		{
-			if (i == intArray.length-1)
-			{
-			System.out.print(intArray[i]);
-			}
-			else 
-			{
-				System.out.print(intArray[i] + ", ");
-			}
+			System.out.print(intArrayBubble[i] + " ");
+		
+			
 			
 		}
 		System.out.println();
-		BubbleSort(intArray);
-		System.out.println("After sort:");
-		for (int i = 0; i < intArray.length ; i++)
+		System.out.println("Before selection sort:");
+		for (int i = 0; i < arrayLength ; i++)
 		{
-			if (i == intArray.length-1)
-			{
-			System.out.print(intArray[i]);
-			}
-			else 
-			{
-				System.out.print(intArray[i] + ", ");
-			}
+			System.out.print(intArraySelection[i] + " ");	
+		}
+		System.out.println();
+		System.out.println("Before insertion sort:");
+		for (int i = 0; i < arrayLength ; i++)
+		{
+			System.out.print(intArrayInsertion[i] + " ");	
+		}
+		System.out.println();
+		bubbleSort(intArrayBubble);
+		selectionSort(intArraySelection);
+		insertionSort(intArrayInsertion);
+		System.out.println("After bubble sort:");
+		for (int i = 0; i < intArrayBubble.length ; i++)
+		{
+			System.out.print(intArrayBubble[i] + " ");
+			
+		}
+		System.out.println();
+		System.out.println("After selection sort:");
+		for (int i = 0; i < intArrayBubble.length ; i++)
+		{
+			System.out.print(intArraySelection[i] + " ");
+			
+		}
+		System.out.println();
+		System.out.println("After insertion sort:");
+		for (int i = 0; i < intArrayBubble.length ; i++)
+		{
+			System.out.print(intArrayInsertion[i] + " ");
 			
 		}
 	}
-	
-	public static void insertionSort(int array[], int n)
+	/**
+	 * 
+	 * @param array
+	 */
+	public static void insertionSort(int[] array)
 	{
-		for (int i = 1; i < n; i++){
-		int j = i;
-		int B = array[i];
-		while ((j > 0) && (array[j-1] > B)){
-		array[j] = array[j-1];
-		j--;
+		for (int i = 1; i < array.length; i++)
+		{
+		int a = i;
+		int b = array[i];
+		while ((a > 0) && (array[a-1] > b))
+		{
+		array[a] = array[a-1];
+		a--;
 		}
-		array[j] = B;
+		array[a] = b;
 		}
 	}
 	
-	
-	public static void BubbleSort(int[] numberArray)
+	/**
+	 * 
+	 * @param numberArray
+	 */
+	public static void bubbleSort(int[] numberArray)
 	{
 		boolean go = true;
 		int tempHold;
@@ -64,23 +101,30 @@ public class Sorts {
 	            go= false;    
 	            for( int i=0;  i < numberArray.length-1;  i++ )
 	            {
-	                   if ( numberArray[i] > numberArray[i+1] )  
+	                   if ( numberArray[i] > numberArray[i+1] )  //if current element is greater than next element   
 	                   {
 	                	   tempHold = numberArray[i];                
 	                	   numberArray[i] = numberArray[i+1];
 	                	   numberArray[i+1] = tempHold;
-	                          go = true;              //shows a swap occurred 
+	                          go = true;//go through while loop again if current element is greater than next element             
 	                  }
 	            }
 	      } 
 	}
-	
-	public static void selectionSort(int[] intArray) {
-	    for (int i=0; i<intArray.length-1; i++) {
-	        for (int j=i+1; j<intArray.length; j++) {
-	            if (intArray[i] > intArray[j]) {
+	/**
+	 * 
+	 * @param intArray
+	 */
+	public static void selectionSort(int[] intArray) 
+	{
+	    for (int i=0; i<intArray.length-1; i++) 
+	    {
+	        for (int j=i+1; j<intArray.length; j++) 
+	        {
+	            if (intArray[i] > intArray[j]) 
+	            {
 	                
-	                int tempHold = intArray[i];//swapping
+	                int tempHold = intArray[i];
 	                intArray[i] = intArray[j];
 	                intArray[j] = tempHold;
 	            }
